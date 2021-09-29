ef76d39a743557bbacf765c8c44746475b25644d
Wagner Passos <wagner_passos@hotmail.com>
2021-09-29 20:46:47 -0300

    Inicializando repositório

--- forEach2.js
+++
@@ -1,9 +0,0 @@
-const aprovados = ['maria', 'pedro', 'joao']
-
-Array.prototype.forEach2 = function(callback){
-    for(let i = 0; i < this.length; i++){
-        callback(this[i], i, this)
-    }
-}  
-
-aprovados.forEach(a => console.log(a)) 
